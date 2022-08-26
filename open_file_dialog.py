9ae731f2b4b391378be192643464722e4d73de53
pvtcompyle <43185139+pvtcompyle@users.noreply.github.com>
2022-08-26 16:27:00 -0500

    cleaned up open_file_dialog comments

--- open_file_dialog.py
+++ open_file_dialog.py
@@ -1,46 +1,50 @@
 def open_file_dialog(
-    _filter='TEXT\0*.txt\0',
-    _customfilter='Other file types\0*.*\0',
-    _initdir='', # either the full path or relative path
-    _envvar = '', # if defining an environment varible as part of your path, define the remaining path in initdir
-    _file="MyFile", DefExt="txt", 
-    _title='Select file to open',
-    _filterindex=0,
-    _multiselect=False # if enabled will allow selecting multiple files and returns a list; [0] is the directory name
+    filter='TEXT\0*.txt\0',
+    customfilter='Other file types\0*.*\0',
+    initdir='',
+    envvar = '', 
+    file="MyFile", DefExt="txt", 
+    title='Select file to open',
+    filterindex=0,
+    multiselect=False 
 ):
     '''
     author: PVTCompyle
     upated: 26-AUG-2022
     DESC: Opens a file dialog box so you can select a file for use in your program. By default, a single filename
-    with directory is returned as a string. If multiselect mode is enabled, a list containg the directory name at [0] 
+    with directory is returned as a string. 
+    
+    If multiselect mode is enabled, a list containg the directory name at [0] 
     and filenames in [1:]. 
+
+    if envvar is defined, the InitialDir will be set to %ENVVAR%\initdir, so set initdir as needed.
     '''
     import win32gui
     import win32con
     import os
 
-    if _envvar:
-        initdir = f'{os.environ[_envvar]}\{initdir}'
+    if envvar:
+        initdir = f'{os.environ[envvar]}\{initdir}'
 
-    if _multiselect:
-        _flags = win32con.OFN_ALLOWMULTISELECT | win32con.OFN_EXPLORER
+    if multiselect:
+        flags = win32con.OFN_ALLOWMULTISELECT | win32con.OFN_EXPLORER
     else:
-        _flags = win32con.OFN_EXPLORER
+        flags = win32con.OFN_EXPLORER
     
     try:
-        fname, _customfilter, flags=win32gui.GetOpenFileNameW(
-            InitialDir = _initdir,
-            Flags = _flags,
-            File = _file,
-            Title = _title,
-            Filter = _filter,
-            CustomFilter = _customfilter,
-            FilterIndex = _filterindex
+        fname, customfilter, flags=win32gui.GetOpenFileNameW(
+            InitialDir = initdir,
+            Flags = flags,
+            File = file,
+            Title = title,
+            Filter = filter,
+            CustomFilter = customfilter,
+            FilterIndex = filterindex
         )
     except Exception as e:
         return f'UserAborted: {e}'
     
-    if _multiselect: # converts fname to a list for easier handling
+    if multiselect: # converts fname to a list for easier handling
         fname = fname.split('\x00')   
 
     return fname
